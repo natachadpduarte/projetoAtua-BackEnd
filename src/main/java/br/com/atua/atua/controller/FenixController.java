e652a181ed52ba86dcce9f296aa4ee30c9d5a0f1
natachadpduarte <natachadpduarte@gmail.com>
2021-05-06 10:42:52 -0300

    Put e delete Fenix

--- src/main/java/br/com/atua/atua/controller/FenixController.java
+++ src/main/java/br/com/atua/atua/controller/FenixController.java
@@ -1,22 +1,22 @@
 package br.com.atua.atua.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-import br.com.atua.atua.repositorio.FenixRepository;
 import br.com.atua.atua.model.Fenix;
+import br.com.atua.atua.repositorio.FenixRepository;
 
 
 @RestController
@@ -48,10 +48,19 @@ public class FenixController {
 		return ResponseEntity.ok(repository.findAllByCnpjContaining(cnpj));
 	}
 	
-	@PostMapping("/cadastrar")
+	@PostMapping("/cadastrarFenix")
 	public ResponseEntity<Fenix> post (@RequestBody Fenix fenix){
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(repository.save(fenix));
 	}
 	
+	@PutMapping("/atualizarFenix")
+	public ResponseEntity<Fenix> put (@RequestBody Fenix fenix) {
+		return ResponseEntity.ok(repository.save(fenix));
+	}
+	
+	@DeleteMapping("/deletarFenix/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
 }
